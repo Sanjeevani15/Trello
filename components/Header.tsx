40ad6473aff75b86749dceaa755a11c9d1e84f7b
Sanjeevani Bhandari <sanjeevanibhandari3@gmail.com>
2023-06-07 06:16:04 +0530

    First commit: 1.Layout of the application and addition of basic utilities and dependencies. 2.Complete Header section

---
+++ components/Header.tsx
@@ -0,0 +1,49 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import Avatar from "react-avatar";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        {/* this self closing div is for gradient color */}
+        <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055D1] rounded-md filter blur-3xl opacity-50 -z-50" />
+        <Image
+          src="https://links.papareact.com/c2cdd5"
+          alt="Trello logo"
+          width={300}
+          height={100}
+          className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
+        />
+        <div className="flex items-center space-x-5 flex-1 w-full justify-end">
+          {/* Search Box */}
+          <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
+            <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+            <input
+              type="text"
+              placeholder="Search"
+              className="flex-1 outline-none p-2"
+            />
+            <button hidden type="submit">
+              Search
+            </button>
+          </form>
+          {/* Avatar */}
+          <Avatar name="Sanjeevani Bhandari" size="50" round color="#0055D1" />
+        </div>
+      </div>
+
+      {/* GPT Suggestion */}
+      <div className="flex items-center justify-center px-5 py-2 md:py-5">
+        <p className="flex items-center text-sm font-light p-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
+          <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
+          GPT is summarising your tasks for the day...
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
