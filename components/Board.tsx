4328e06b98dc54b3392623a83edc9c82ed99ea23
Sanjeevani Bhandari <sanjeevanibhandari3@gmail.com>
2023-06-07 22:15:38 +0530

    1.Creating the Board Component which have all three columns- Todo, InProgress and Done. 2.Creating the Column Component in which all the todos based on the columns are arranged. 3.Creating the TodoCard Component- without image addition. Here atm only the title of todos will show.

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+"use client";
+import { useBoardStore } from "@/store/BoardStore";
+import React, { useEffect } from "react";
+import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
+import Column from "./Column";
 
 const Board = () => {
-  return <div>Board</div>;
+  const [getBoard, board] = useBoardStore((state) => [
+    state.getBoard,
+    state.board,
+  ]);
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
+  const handleOnDragEnd = (result: DropResult) => {
+    
+  }
+
+  return (
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => (
+          <div
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
+          >
+            {/* rendering all the columns here */}
+            {/* {provided.placeholder} */}
+
+            {Array.from(board.columns.entries()).map(([id, column], index) => (
+              <Column key={id} id={id} todos={column.todos} index={index} />
+            ))}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
 };
 
 export default Board;
---
+++ components/TodoCard.tsx
@@ -0,0 +1,43 @@
+import { XCircleIcon } from "@heroicons/react/24/solid";
+import React from "react";
+import {
+  DraggableProvidedDragHandleProps,
+  DraggableProvidedDraggableProps,
+} from "react-beautiful-dnd";
+
+type Props = {
+  todo: Todo;
+  index: number;
+  id: TypedColumn;
+  innerRef: (element: HTMLElement | null) => void;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps: DraggableProvidedDragHandleProps | null | undefined;
+};
+
+const TodoCard = ({
+  todo,
+  index,
+  id,
+  innerRef,
+  draggableProps,
+  dragHandleProps,
+}: Props) => {
+  return (
+    <div
+      className="bg-white rounded-md space-y-2 drop-shadow-md"
+      {...draggableProps}
+      {...dragHandleProps}
+      ref={innerRef}
+    >
+      <div className="flex justify-between items-center p-5">
+        <p>{todo.title}</p>
+        <button className="text-red-500 hover:text-red-600">
+          <XCircleIcon className="ml-5 h-8 w-8" />
+        </button>
+      </div>
+      {/* Add image here */}
+    </div>
+  );
+};
+
+export default TodoCard;
