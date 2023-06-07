2babbd4daade59ac33750ef6bd1b238ac1d8f6a6
Sanjeevani Bhandari <sanjeevanibhandari3@gmail.com>
2023-06-07 20:23:14 +0530

    zustand setup, appwrite setup and zustand store initialization

---
+++ typings.d.ts
@@ -0,0 +1,25 @@
+interface Board {
+  // id: string;
+  // title: string;
+  // description: string;
+  columns: Map<TypedColumn, Column>;
+}
+
+type TypedColumn = "todo" | "inprogress" | "done";
+interface Column {
+  id: TypedColumn;
+  todos: Todo[];
+}
+
+interface Todo {
+  $id: string;
+  $createdAt: string;
+  title: string;
+  status: TypedColumn;
+  image?: Image;
+}
+
+interface Image {
+  bucketId: string;
+  fileId: string;
+}
